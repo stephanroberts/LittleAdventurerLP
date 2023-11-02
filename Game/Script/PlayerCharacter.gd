6b87afcab4887683c1f6c22f7eb9e71ab3c453cd
stephanrobert <stephanrobert@posteo.de>
2023-11-02 14:24:14 +0100

    started the character controller

---
+++ Game/Script/PlayerCharacter.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+@onready var visual: Node3D = $VisualNode
+@onready var animationPlayer: AnimationPlayer = $VisualNode/AnimationPlayer
+
+const SPEED = 5.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= 1
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		animationPlayer.play('LittleAdventurerAndie_Run')
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		animationPlayer.play('LittleAdventurerAndie_Idel')
+		
+	if velocity.length() > 0.2:
+		var lookDirection = Vector2(velocity.z, velocity.x)
+		visual.rotation.y = lookDirection.angle()
+
+	move_and_slide()
